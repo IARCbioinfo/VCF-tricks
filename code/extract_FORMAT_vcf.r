78ae9509a32290313818776ab97c713228c038df
Tiffany Delhomme <delhommet@students.iarc.fr>
2017-10-24 14:20:22 +0200

    use chunks to control for memory

--- code/extract_FORMAT_vcf.r
+++ code/extract_FORMAT_vcf.r
@@ -9,15 +9,29 @@ args <- argsL;rm(argsL)
 if(is.null(args$input_vcf))            {stop("no input VCF file")} else {input_vcf = args$input_vcf}
 if(is.null(args$field))                {stop("no FORMAT field to query")} else {field=args$field}
 if(is.null(args$out_txt))              {out_txt = paste(gsub(".vcf.gz","",input_vcf),"_",field,"_extract.txt",sep="")} else {out_txt=args$out_txt}
-if(is.null(args$nb_mut))               {nb_mut = 1000000} else {nb_mut = as.numeric(args$nb_mut)}
+if(is.null(args$nb_mut))               {nb_mut = 1000} else {nb_mut = as.numeric(args$nb_mut)}
 
 vcf <- open(VcfFile(input_vcf,  yieldSize=nb_mut))
 vcf_chunk = readVcf(vcf, "hg19")
-`#CHR` = as.character(seqnames(rowRanges(vcf_chunk)))
-`#START` = start(ranges(rowRanges(vcf_chunk,"seqnames")))
-`#REF` = as.character(rowRanges(vcf_chunk)$REF)
-`#ALT` = as.character(unlist(rowRanges(vcf_chunk)$ALT))
 
-res_matrix = rbind(`#CHR`, `#START`, `#REF`, `#ALT`, t(geno(vcf_chunk, field)))
+`#CHR` = c()
+`#START` = c()
+`#REF` = c()
+`#ALT` = c()
+`#FILTER` = c()
+  
+while(dim(vcf_chunk)[1] != 0) {
+  `#CHR` = c(`#CHR`, as.character(seqnames(rowRanges(vcf_chunk))))
+  `#START` = c(`#START`, start(ranges(rowRanges(vcf_chunk,"seqnames"))))
+  `#REF` = c(`#REF`, as.character(rowRanges(vcf_chunk)$REF))
+  `#ALT` = c(`#ALT`, as.character(unlist(rowRanges(vcf_chunk)$ALT)))
+  `#FILTER` = as.character(unlist(rowRanges(vcf_chunk)$FILTER))
+  if( exists("total_vcf_chunk") ) {
+    total_vcf_chunk = cbind(total_vcf_chunk, t(geno(vcf_chunk, field)))
+  } else { total_vcf_chunk = t(geno(vcf_chunk, field)) }
+  vcf_chunk = readVcf(vcf, "hg19")
+}
 
-write.table(data.frame("." = rownames(res_matrix), res_matrix, check.names = F), file = out_txt, row.names=FALSE, sep="\t", quote = F)
+res_matrix = rbind(`#CHR`, `#START`, `#REF`, `#ALT`, `#FILTER`, total_vcf_chunk)
+
+write.table(data.frame("#CHR:POS" = rownames(res_matrix), res_matrix, check.names = F), file = out_txt, row.names=FALSE, sep="\t", quote = F)
