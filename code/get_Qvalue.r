c05f7dd3122d930e9ae58969b5b12234b28de29c
tdelhomme <delhommet@students.iarc.fr>
2018-01-23 09:08:40 +0100

    useless input parameter in toQvalue function

--- code/get_Qvalue.r
+++ code/get_Qvalue.r
@@ -5,13 +5,15 @@
 # mut is in the form: chr:start_ref/alt
 # sm corresponds to one sample as it is defined in the VCF file
 
-toQvalue <- function(DPi, AOi, all_DP, all_AO, mu_est, sig_est, vcf_chunk, sm, mut){
-  my_col = which(colnames(geno(vcf_chunk,"GT"))==sm)
-  my_row = which(rownames(geno(vcf_chunk,"GT"))==mut)
-  mu_est = info(vcf_chunk[my_row,])$ERR
-  sig_est = info(vcf_chunk[my_row,])$SIG
-  all_DP = unlist(as.list(geno(vcf_chunk[my_row,],"DP")))
-  all_AO = unlist(as.list(geno(vcf_chunk[my_row,],"AO")))
+toQvalue <- function(vcf_chunk, sm, mut){
+  id_sm = which(colnames(geno(vcf_chunk,"GT"))==sm)
+  id_mut = which(rownames(geno(vcf_chunk,"GT"))==mut)
+  mu_est = info(vcf_chunk[id_mut,])$ERR
+  sig_est = info(vcf_chunk[id_mut,])$SIG
+  all_DP = unlist(as.list(geno(vcf_chunk[id_mut,],"DP")))
+  all_AO = unlist(as.list(geno(vcf_chunk[id_mut,],"AO")))
+  DPi = all_DP[id_sm]
+  AOi = all_AO[id_sm]
 
   if(AOi>DPi) return(0)
   res = unlist(-10*log10(p.adjust((dnbinom(c(all_AO,AOi),size=1/sig_est,mu=mu_est*c(all_DP,DPi)) +
